24321ea59bd6be1d9eb72fffb3e7109646b6183f
Ricado-Xv <57443417+Ricado-Xv@users.noreply.github.com>
2023-04-02 20:06:08 +0800

    Add files via upload

---
+++ handle.py
@@ -0,0 +1,48 @@
+import hashlib
+import web
+import xml.etree.ElementTree as ET
+from first_python_server import receive, reply
+
+
+class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "xxxx" #请按照公众平台官网\基本配置中信息填写
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1(''.join(list).encode('utf-8'))
+            hashcode = sha1.hexdigest()
+            print("handle/GET func: hashcode, signature: ", hashcode, signature)
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception as Argument:
+            return Argument
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print("Handle Post webdata is ", webData)
+            #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                # content = "test"
+                xml_root = ET.fromstring(webData)
+                content = xml_root.find('Content').text if xml_root.find('Content') is not None else ''
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print("暂且不处理")
+                return "success"
+        except Exception as Argment:
+            return Argment
